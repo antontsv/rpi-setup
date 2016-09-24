3a722c55ac45ab90fa2b26dec0489f5bf10273a0
Anton Tsviatkou <a.tsviatkou@gmail.com>
2016-09-23 20:10:18 -0700

    Add optional gcc-6.1.0 setup

--- setup-homebridge.bash
+++ setup-homebridge.bash
@@ -1,9 +1,24 @@
 #!/bin/bash
 
-sudo apt-get install libavahi-compat-libdnssd-dev screen
+if [ "$1" = "--install-gcc-6.1.0"];then
+    git clone https://bitbucket.org/sol_prog/raspberry-pi-gcc-6.1.0-binary
+    cd raspberry-pi-gcc-6.1.0-binary/
+    7z e gcc-6.1.0.tar.7z
+    tar -xf gcc-6.1.0.tar
+    sudo mv gcc-6.1.0 /usr/local/gcc-6.1.0
+    cd ~
+    rm -rf raspberry-pi-gcc-6.1.0-binar
+    # todo link gcc binary or use alternatives
+fi;
+
+sudo apt-get update
+sudo apt-get install -y p7zip-full libavahi-compat-libdnssd-dev screen
+
+source setup-nodejs.bash
 
 sudo npm install -g --unsafe-perm homebridge hap-nodejs node-gyp
 cd /usr/local/lib/node_modules/
 sudo npm install --unsafe-perm bignum
 cd /usr/local/lib/node_modules/hap-nodejs/node_modules/mdns
 sudo node-gyp BUILDTYPE=Release rebuild
+echo 'Done'
