d8d1d1a12cd2f440b484409e299661c9a2a59f3d
Anton Tsviatkou <a.tsviatkou@gmail.com>
2016-10-29 21:24:33 -0700

    Shellckeck: copy directly from target directory in setup-nodejs.bash

--- setup-nodejs.bash
+++ setup-nodejs.bash
@@ -3,6 +3,5 @@
 target='node-v6.6.0-linux-armv6l'
 wget "https://nodejs.org/dist/latest-v6.x/$target.tar.gz"
 tar -xvf "$target.tar.gz"
-cd "$target"
-sudo cp -R * /usr/local/
+sudo cp -R "$target/"* /usr/local/
 node -v
