31126b3a14fa5a57a84f6b1633c6ceb902705091
Anton Tsviatkou <a.tsviatkou@gmail.com>
2018-11-24 21:22:16 -0800

    Use nodejs 6.14.4, since previous version is outdated

--- setup-nodejs.bash
+++ setup-nodejs.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-target='node-v6.6.0-linux-armv6l'
+target='node-v6.14.4-linux-armv6l'
 wget "https://nodejs.org/dist/latest-v6.x/$target.tar.gz"
 tar -xvf "$target.tar.gz"
 sudo cp -R "$target/"* /usr/local/
